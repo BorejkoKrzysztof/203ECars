c7934fc15535e2dd328acb40e882be6f7dc32bdf
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-08-15 16:37:07 +0200

    Work on availableCars Page part 1

--- src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/AvailableCarsPage.js
+++ src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/AvailableCarsPage.js
@@ -1,42 +1,62 @@
 import React, { useState } from 'react'
 import styles from './AvailableCarsPage.module.css'
-import { FaFilter } from 'react-icons/fa'
+import { FaFilter, FaWindowClose } from 'react-icons/fa'
 import { AiOutlineArrowDown } from 'react-icons/ai'
 
 function AvailableCarsPage() {
 
-  const [formState, setFormState] = useState(false)
+  const [filterFormState, setFilterFormState] = useState(false)
+  const [locationFormState, setLocationFormState] = useState(false)
 
 
   return (
     <div className={styles.availableCarsWrapper}>
       <div className={styles.AvailableCarsContent}>
         <div className={styles.filtersWrapper}>
-          <div className={styles.locationsFilterContent}>
+          <div className={ !locationFormState ? 
+                                    styles.locationsFilterContent
+                                              :
+                                    `${styles.locationsFilterContent} ${styles.locationsFilterContentActive}`}>
             <div className={styles.locationInfo}>
-
+              DROP ON
             </div>
             <div className={styles.locationInfo}>
-
+              DROP OFF
             </div>
             <div className={styles.locationFormOpenButtonWrapper}>
-              <button>Open</button>
+              <button onClick={() => { setLocationFormState(true) }}>Open</button>
             </div>
           </div>
-          <div className={!formState ? 
+          
+
+          <div className={!locationFormState ? 
+                              `${styles.locationFormWrapper}`
+                                              :
+                              `${styles.locationFormWrapper} ${styles.locationFormWrapperActive}`}>
+              <div className={styles.closeLoactionFormButtonArea}>
+                <button className={styles.closeLocationFormButton}
+                          onClick={() => { setLocationFormState(false) }}>
+                  <FaWindowClose />
+                </button>
+              </div>
+              LOCATION FORM
+          </div>
+
+
+          <div className={!filterFormState ? 
                             `${styles.otherSettingsFilterContent}` 
                                       : 
                             `${styles.otherSettingsFilterContent} ${styles.otherSettingsFilterContentActive}`}>
-            <div className={styles.otherSettingsFilterBar} onClick={() => { setFormState(!formState) }}>
+            <div className={styles.otherSettingsFilterBar} onClick={() => { setFilterFormState(!filterFormState) }}>
                 <div className={styles.filterBarContent}>
                   <FaFilter />
                   <h3>Filtruj</h3>
                 </div>
                 <AiOutlineArrowDown 
-                    className={!formState ? `${styles.filterArrow}` : `${styles.filterArrow} ${styles.filterArrowActive}`}/>
+                    className={!filterFormState ? `${styles.filterArrow}` : `${styles.filterArrow} ${styles.filterArrowActive}`}/>
             </div>
             <div className={styles.filterFormWrapper}>
-
+                FILTER FORM
             </div>
           </div>
         </div>
@@ -45,16 +65,16 @@ function AvailableCarsPage() {
 
 
           <div className={styles.carItem}>
-
+              CAR ITEM
           </div>
           <div className={styles.carItem}>
-
+              CAR ITEM
           </div>
           <div className={styles.carItem}>
-
+              CAR ITEM
           </div>
           <div className={styles.carItem}>
-
+              CAR ITEM
           </div>
 
         </div>
