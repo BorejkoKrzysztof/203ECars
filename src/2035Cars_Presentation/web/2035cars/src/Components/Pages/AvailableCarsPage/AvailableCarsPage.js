6eb74d2009bb4e31f42718af250cd457c082d152
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-08-16 20:01:12 +0200

    style othersettingsFilter

--- src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/AvailableCarsPage.js
+++ src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/AvailableCarsPage.js
@@ -1,15 +1,39 @@
 import React, { useState } from 'react'
 import styles from './AvailableCarsPage.module.css'
 import { FaFilter, FaWindowClose } from 'react-icons/fa'
-import { AiOutlineArrowDown } from 'react-icons/ai'
+import { AiOutlineArrowDown, AiOutlineCheckSquare, AiFillCheckSquare } from 'react-icons/ai'
 import { IoLocationSharp, IoTimeSharp } from 'react-icons/io5'
 import { BsFillCalendarDateFill } from 'react-icons/bs'
+import Slider from '@mui/material/Slider'
 
 function AvailableCarsPage() {
 
-  const [filterFormState, setFilterFormState] = useState(false)
+  const [filterFormState, setFilterFormState] = useState(true)
   const [locationFormState, setLocationFormState] = useState(false)
 
+  const [suvCarTypeChecked, setSuvCarTypeChecked] = useState(false)
+  const [sportCarTypeChecked, setSportCarTypeChecked] = useState(false)
+  const [convertibleCarTypeChecked, setConvertibleCarTypeChecked] = useState(false)
+  const [sedanCarTypeChecked, setSedanCarTypeChecked] = useState(false)
+
+  const [airConditioningChecked, setAirConditioningChecked] = useState(false)
+  const [heatedSeatChecked, setHeatedSeatChecked] = useState(false)
+  const [automaticGearBoxChecked, setAutomaticGearBoxChecked] = useState(false)
+  const [navigationChecked, setNavigationChecked] = useState(false)
+  
+  const [hybridFuelChecked, setHybridFuelChecked] = useState(false)
+  const [electricFuelChecked, setElectricFuelChecked] = useState(false)
+
+  const [sliderVal, setSliderVal] = useState([0,100])
+
+  const updateSlider = (e, data) => {
+    setSliderVal(data)
+
+    // console.log(sliderVal[0])
+    // console.log(sliderVal[1])
+  }
+
+  const getText = (value) => `${value}`
 
   return (
     <div className={styles.availableCarsWrapper}>
@@ -113,7 +137,122 @@ function AvailableCarsPage() {
                     className={!filterFormState ? `${styles.filterArrow}` : `${styles.filterArrow} ${styles.filterArrowActive}`}/>
             </div>
             <div className={styles.filterFormWrapper}>
-                FILTER FORM
+                
+                <div className={styles.filterFormOptionPart}>
+                  <h1>Cena: </h1>
+                  <div className={styles.sliderArea}>
+                    <Slider
+                      step={10}
+                      value={sliderVal}
+                      onChange={updateSlider}
+                      getAriaValueText = {getText}
+                      valueLabelDisplay='on'
+                    />
+                  </div>
+                  <h4>Od: {sliderVal[0]} Do: {sliderVal[1]}</h4>
+                </div>
+                <div className={styles.filterFormOptionPart}>
+                  <h1>Typ nadwozia:</h1>
+                  <p onClick={() => setSuvCarTypeChecked(!suvCarTypeChecked)}>
+                    Suv
+                    <span>
+                      {
+                          !suvCarTypeChecked ? <AiOutlineCheckSquare /> : <AiFillCheckSquare />
+                      }  
+                    </span>  
+                  </p>
+                  <p onClick={() => setSportCarTypeChecked(!sportCarTypeChecked)}>
+                    Sportowy
+                    <span>
+                      {
+                          !sportCarTypeChecked ? <AiOutlineCheckSquare /> : <AiFillCheckSquare />
+                      }  
+                    </span>  
+                  </p>
+                  <p onClick={() => setConvertibleCarTypeChecked(!convertibleCarTypeChecked)}>
+                    Kabriolet
+                    <span>
+                      {
+                          !convertibleCarTypeChecked ? <AiOutlineCheckSquare /> : <AiFillCheckSquare />
+                      }  
+                    </span>  
+                  </p>
+                  <p onClick={() => setSedanCarTypeChecked(!sedanCarTypeChecked)}>
+                    Sedan
+                    <span>
+                      {
+                          !sedanCarTypeChecked ? <AiOutlineCheckSquare /> : <AiFillCheckSquare />
+                      }  
+                    </span>  
+                  </p>
+                </div>
+                <div className={styles.filterFormOptionPart}>
+                  <h1>Wyposażenie</h1>
+                  <p onClick={() => setAirConditioningChecked(!airConditioningChecked)}>
+                    Klimatyzacja
+                    <span>
+                      {
+                          !airConditioningChecked ? <AiOutlineCheckSquare /> : <AiFillCheckSquare />
+                      }  
+                    </span>  
+                  </p>
+                  <p onClick={() => setHeatedSeatChecked(!heatedSeatChecked)}>
+                    Podgrzewane fotele
+                    <span>
+                      {
+                          !heatedSeatChecked ? <AiOutlineCheckSquare /> : <AiFillCheckSquare />
+                      }  
+                    </span>  
+                  </p>
+                  <p onClick={() => setAutomaticGearBoxChecked(!automaticGearBoxChecked)}>
+                    Automatyczna skrzynia biegów
+                    <span>
+                      {
+                          !automaticGearBoxChecked ? <AiOutlineCheckSquare /> : <AiFillCheckSquare />
+                      }  
+                    </span>  
+                  </p>
+                  <p onClick={() => setNavigationChecked(!navigationChecked)}>
+                    Wbudowana nawigacja
+                    <span>
+                      {
+                          !navigationChecked ? <AiOutlineCheckSquare /> : <AiFillCheckSquare />
+                      }  
+                    </span>  
+                  </p>
+                </div>
+                <div className={styles.filterFormOptionPart}>
+                  <h1>Typ napędu</h1>
+                  <p onClick={() => setHybridFuelChecked(!hybridFuelChecked)}>
+                    Hybrydowy
+                    <span>
+                      {
+                          !hybridFuelChecked ? <AiOutlineCheckSquare /> : <AiFillCheckSquare />
+                      }  
+                    </span>  
+                  </p>
+                  <p onClick={() => setElectricFuelChecked(!electricFuelChecked)}>
+                    W pełni elektryczny
+                    <span>
+                      {
+                          !electricFuelChecked ? <AiOutlineCheckSquare /> : <AiFillCheckSquare />
+                      }  
+                    </span>  
+                  </p>
+                </div>
+                <div className={styles.filterFormOptionPart}>
+                  <h1>Ilość drzwi</h1>
+                  <select>
+                      <option value={1}>2/3</option>
+                      <option value={2}>4/5</option>
+                  </select>
+                  <h1>Ilość miejsc</h1>
+                  <select>
+                      <option value={1}>2</option>
+                      <option value={2}>4</option>
+                      <option value={5}>5</option>
+                  </select>
+                </div>
             </div>
           </div>
         </div>
