9334e4198fbc2104ec1bdb5f6a3d36644fa50951
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-08-19 21:28:46 +0200

    Style CarItem component

--- src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/SubComponents/CarContent/CarItem/CarItem.js
+++ src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/SubComponents/CarContent/CarItem/CarItem.js
@@ -11,48 +11,58 @@ function CarItem() {
             <div className={styles.carItemTitle}>
                 <h1>Marka Model</h1>
             </div>
-            <div className={styles.mainPart}>
-                <img className={styles.carItemImage}
-                    src={process.env.PUBLIC_URL + '/Images/ExampleCars/Przechwytywanie.jpg'}
-                    alt={`car`} />
+            <div className={styles.carItemMiddle}>
+                <div className={styles.mainPart}>
+                    <img className={styles.carItemImage}
+                        src={process.env.PUBLIC_URL + '/Images/ExampleCars/Przechwytywanie.jpg'}
+                        alt={`car`} />
+                </div>
+                <div className={styles.carItemDescription}>
+                    <p>
+                        <span>
+                            <GiCarSeat />
+                        </span>
+                        {`5`} ilość siedzeń
+                    </p>
+                    <p>
+                        <span>
+                            <GiCarDoor />
+                        </span>
+                        {`5`} ilość drzwi
+                    </p>
+                    <p>
+                        <span>
+                            <RiTempColdFill />
+                        </span>
+                        Automatyczna klimatyzacja
+                    </p>
+                    <p>
+                        <span>
+                            <TbManualGearbox />
+                        </span>
+                        Automatyczna skrzynia biegów
+                    </p>
+                    <p>
+                        <span>
+                            <GiHotSurface />
+                        </span>
+                        Podgrzewane fotele
+                    </p>
+                    <p>
+                        <span>
+                            <RiNavigationFill />
+                        </span>
+                        Wbudowana nawigacja
+                    </p>
+                </div>
             </div>
-            <div className={styles.carItemDescription}>
-                <p>
-                    <span>
-                        <GiCarSeat />
-                    </span>
-                    {`5`} ilość siedzeń
-                </p>
-                <p>
-                    <span>
-                        <GiCarDoor />
-                    </span>
-                    {`5`} ilość drzwi
-                </p>
-                <p>
-                    <span>
-                        <RiTempColdFill />
-                    </span>
-                    Automatyczna klimatyzacja
-                </p>
-                <p>
-                    <span>
-                        <TbManualGearbox />
-                    </span>
-                    Automatyczna skrzynia biegów
-                </p>
-                <p>
-                    <span>
-                        <GiHotSurface />
-                    </span>
-                    Podgrzewane fotele
-                </p>
-                <p>
-                    <span>
-                        <RiNavigationFill />
-                    </span>
-                    Wbudowana nawigacja
-                </p>
+            <div className={styles.priceArea}>
+                    <p>
+                        Za {`3`} dni:
+                    </p>
+                    <h3>
+                        250.00 PLN
+                    </h3>
             </div>
             <div className={styles.rentCarButtonArea}>
                 <button className={styles.rentCarButton}>Wypożycz</button>
