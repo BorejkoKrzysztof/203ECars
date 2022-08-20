310642c0144546726284de5ba9a8b20c7fd6fb14
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-08-20 17:47:40 +0200

    Add title to AvailableCars Page

--- src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/AvailableCarsPage.js
+++ src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/AvailableCarsPage.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './AvailableCarsPage.module.css'
 import CarContent from './SubComponents/CarContent/CarContent'
 import Filters from './SubComponents/Filters/Filters'
 
 function AvailableCarsPage() {
+
+  useEffect(() => {
+    document.title = 'Dostępne samochody'
+  }, [])
 
   return (
     <div className={styles.availableCarsWrapper}>
