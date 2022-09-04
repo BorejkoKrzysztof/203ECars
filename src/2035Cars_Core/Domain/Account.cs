e6665eee33db4cae0ad3d49fa88def6b45cd64fd
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-04 16:49:16 +0200

    Update Account Identity

--- src/2035Cars_Core/Domain/Account.cs
+++ src/2035Cars_Core/Domain/Account.cs
@@ -3,9 +3,31 @@ namespace _2035Cars_Core.Domain;
 
 public class Account : BaseIdentity
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string EmailAddress { get; set; }
-    public string Password { get; set; }
-    public string ConfirmPassword { get; set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string EmailAddress { get; private set; }
+    public string Password { get; private set; }
+    public string Role { get; private set; }
+    public DateTime RegisteredAt { get; private set; }
+    public DateTime LastLoggedAt { get; private set; }
+
+    public Account()
+    {
+        
+    }
+
+    public Account Create(string firstName, string lastName, string emailAddress, 
+                            string password, string role)
+    {
+        return new Account
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            EmailAddress = emailAddress,
+            Password = password,
+            Role = role,
+            RegisteredAt = DateTime.UtcNow,
+            LastLoggedAt = DateTime.UtcNow
+        };
+    }
 }
