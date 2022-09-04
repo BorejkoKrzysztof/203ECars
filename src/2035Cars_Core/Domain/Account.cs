21c3a3e70209ae8cfb44b3bf1f291011ef8796bc
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-04 16:54:08 +0200

    Improve Account Identity

--- src/2035Cars_Core/Domain/Account.cs
+++ src/2035Cars_Core/Domain/Account.cs
@@ -1,14 +1,39 @@
+using System.ComponentModel.DataAnnotations;
 
 namespace _2035Cars_Core.Domain;
 
 public class Account : BaseIdentity
 {
+    [Required]
+    [MinLength(3)]
+    [MaxLength(30)]
     public string FirstName { get; private set; }
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(30)]
     public string LastName { get; private set; }
+
+    [Required]
+    [EmailAddress]
+    [MinLength(5)]
+    [MaxLength(70)]
     public string EmailAddress { get; private set; }
+
+    [Required]
+    [MinLength(10)]
+    [MaxLength(100)]
     public string Password { get; private set; }
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(30)]
     public string Role { get; private set; }
+
+    [Required]
     public DateTime RegisteredAt { get; private set; }
+
+    [Required]
     public DateTime LastLoggedAt { get; private set; }
 
     public Account()
