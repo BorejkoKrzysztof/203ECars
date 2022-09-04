5617d2a048eb29598719905084d3096873be9fce
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-04 18:14:00 +0200

    Add Create Account method

--- src/2035Cars_Core/Repositories/IAccountRepository.cs
+++ src/2035Cars_Core/Repositories/IAccountRepository.cs
@@ -1,6 +1,8 @@
+using _2035Cars_Core.Domain;
+
 namespace _2035Cars_Core.Repositories;
 
 public interface IAccountRepository
 {
-    
+    Task CreateAccount(Account account);
 }
--- src/2035Cars_Infrastructure/Repositories/AccountRepository.cs
+++ src/2035Cars_Infrastructure/Repositories/AccountRepository.cs
@@ -1,8 +1,12 @@
+using _2035Cars_Core.Domain;
 using _2035Cars_Core.Repositories;
 
 namespace _2035Cars_Infrastructure.Repositories;
 
-public class AccountRepository: IAccountRepository
+public class AccountRepository : IAccountRepository
 {
-    
+    public Task CreateAccount(Account account)
+    {
+        throw new NotImplementedException();
+    }
 }
