c991c00b3b08dfc7de734ef46a0306623fdc73e5
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-04 16:37:14 +0200

    Create AccountService

---
+++ src/2035Cars_Infrastructure/Services/AccountService.cs
@@ -0,0 +1,6 @@
+namespace _2035Cars_Infrastructure.Services;
+
+public class AccountService : IAccountService
+{
+
+}
---
+++ src/2035Cars_Infrastructure/Services/IAccountService.cs
@@ -0,0 +1,6 @@
+namespace _2035Cars_Infrastructure.Services;
+
+public interface IAccountService
+{
+    
+}
