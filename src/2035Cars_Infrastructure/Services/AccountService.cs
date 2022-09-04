2a8dcaa11485b38b04961651b7f63bfa78bd14e6
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-04 18:13:26 +0200

    Add AccountServices

--- src/2035Cars_Infrastructure/Services/AccountService.cs
+++ src/2035Cars_Infrastructure/Services/AccountService.cs
@@ -1,6 +1,19 @@
+using _2035Cars_Core.Repositories;
+using _2035Cars_Infrastructure.Commands.Account;
+
 namespace _2035Cars_Infrastructure.Services;
 
 public class AccountService : IAccountService
 {
+    private readonly IAccountRepository _repository;
 
+    public AccountService(IAccountRepository repository)
+    {
+        this._repository = repository;
+    }
+    public Task CreateAccount(RegisterRequestAccount accountInfo)
+    {
+        // this._repository.CreateAccount()
+        throw new NotImplementedException();
+    }
 }
--- src/2035Cars_Infrastructure/Services/IAccountService.cs
+++ src/2035Cars_Infrastructure/Services/IAccountService.cs
@@ -1,6 +1,8 @@
+using _2035Cars_Infrastructure.Commands.Account;
+
 namespace _2035Cars_Infrastructure.Services;
 
 public interface IAccountService
 {
-    
+    Task CreateAccount(RegisterRequestAccount accountInfo);
 }
