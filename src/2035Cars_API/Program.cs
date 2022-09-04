b3e3519a50682035fdf6778b8f05ca329cfe96e7
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-04 16:31:46 +0200

    Register AccountRepository service

--- src/2035Cars_API/Program.cs
+++ src/2035Cars_API/Program.cs
@@ -1,3 +1,6 @@
+using _2035Cars_Core.Repositories;
+using _2035Cars_Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 
 var app = builder.Build();
 
