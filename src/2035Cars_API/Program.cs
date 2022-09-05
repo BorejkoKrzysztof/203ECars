406c0d466d439e27c69a841495274ba95deda691
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-05 18:31:20 +0200

    Add JWT in services

--- src/2035Cars_API/Program.cs
+++ src/2035Cars_API/Program.cs
@@ -1,6 +1,9 @@
+using System.Text;
 using _2035Cars_Core.Repositories;
 using _2035Cars_Infrastructure.Repositories;
 using _2035Cars_Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//  Adding Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+// Adding Jwt Bearer
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JwtAuthentication:JwtAudience"],
+        ValidIssuer = builder.Configuration["JwtAuthentication:JwtIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
+                                    (builder.Configuration["JwtAuthentication:JwtKey"]))
+    };
+});
 
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 builder.Services.AddScoped<IAccountService, AccountService>();
