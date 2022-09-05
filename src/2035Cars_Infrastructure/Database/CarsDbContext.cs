42d51aef8b6a363f691f02acde910694459ab40e
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-05 15:14:45 +0200

    Add db context

---
+++ src/2035Cars_Infrastructure/Database/CarsDbContext.cs
@@ -0,0 +1,14 @@
+using _2035Cars_Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2035Cars_Infrastructure.Database;
+
+public class CarsDbContext : DbContext
+{
+    public CarsDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Account> Accounts { get; set; }
+}
