86412f339f707008258396569a16991f16655fb9
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-05 18:29:25 +0200

    Improve Account Service

--- src/2035Cars_Infrastructure/Services/IAccountService.cs
+++ src/2035Cars_Infrastructure/Services/IAccountService.cs
@@ -1,9 +1,11 @@
 using _2035Cars_Infrastructure.Commands.Account;
+using _2035Cars_Infrastructure.DTO;
 
 namespace _2035Cars_Infrastructure.Services;
 
 public interface IAccountService
 {
+    Task<TokenDTO> Login(string emailAddress, string password);
     Task RegisterAccount(string firstName, string lastName, string emailAddress,
                         string password, string confirmPassword);
 }
