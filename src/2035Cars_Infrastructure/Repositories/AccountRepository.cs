5ca66e8229bb02f7ff45deabc19b751d415e2493
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-05 18:30:43 +0200

    Add GetByEmail Feature

--- src/2035Cars_Infrastructure/Repositories/AccountRepository.cs
+++ src/2035Cars_Infrastructure/Repositories/AccountRepository.cs
@@ -1,6 +1,7 @@
 using _2035Cars_Core.Domain;
 using _2035Cars_Core.Repositories;
 using _2035Cars_Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace _2035Cars_Infrastructure.Repositories;
 
@@ -17,5 +18,13 @@ public class AccountRepository : IAccountRepository
     {
         await this.context.Accounts.AddAsync(account);
         await this.context.SaveChangesAsync();
+    }
+
+    public async Task<Account> GetByEmail(string emailAddress)
+    {
+        var account = this.context.Accounts
+                        .FirstOrDefault(x => string.Equals(x.EmailAddress, emailAddress));
+
+        return await Task.FromResult(account!);
     }
 }
