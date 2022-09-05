62b0f228251761d11b7e71f4e5368b56dec8d375
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-05 18:30:31 +0200

    Add Jwt

---
+++ src/2035Cars_Infrastructure/DTO/JwtDTO.cs
@@ -0,0 +1,7 @@
+namespace _2035Cars_Infrastructure.DTO;
+
+public class JwtDTO
+{
+    public string Token { get; set; }
+    public long Expires { get; set; }
+}
---
+++ src/2035Cars_Infrastructure/DTO/TokenDTO.cs
@@ -0,0 +1,6 @@
+namespace _2035Cars_Infrastructure.DTO;
+
+public class TokenDTO : JwtDTO
+{
+    public string RefreshToken { get; set; }
+}
