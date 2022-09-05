2a76b9d2b226edd0ee6e5220c9d966ff889431cf
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-05 15:41:50 +0200

    Create method

--- src/2035Cars_Infrastructure/Repositories/AccountRepository.cs
+++ src/2035Cars_Infrastructure/Repositories/AccountRepository.cs
@@ -1,12 +1,21 @@
 using _2035Cars_Core.Domain;
 using _2035Cars_Core.Repositories;
+using _2035Cars_Infrastructure.Database;
 
 namespace _2035Cars_Infrastructure.Repositories;
 
 public class AccountRepository : IAccountRepository
 {
-    public Task CreateAccount(Account account)
+    private readonly CarsDbContext context;
+
+    public AccountRepository(CarsDbContext context)
     {
-        throw new NotImplementedException();
+        this.context = context;
+    }
+
+    public async Task CreateAccount(Account account)
+    {
+        await this.context.Accounts.AddAsync(account);
+        await this.context.SaveChangesAsync();
     }
 }
