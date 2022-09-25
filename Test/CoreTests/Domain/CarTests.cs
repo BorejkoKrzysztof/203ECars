aaad8abe09dd65ee446aa9991b11b5525148e372
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-25 15:21:29 +0200

    Create Car Tests

---
+++ Test/CoreTests/Domain/CarTests.cs
@@ -0,0 +1,116 @@
+using _2035Cars_Core.Domain;
+using _2035Cars_Core.Enums;
+using _2035Cars_Core.ValueObjects;
+
+namespace Test.CoreTests.Domain
+{
+    public class CarTests
+    {
+        private string brand;
+        private string model;
+        private CarType carType;
+        private CarEquipment carEquipment;
+        private DriveOfCar driveOfCar;
+        private int amountOfDoor;
+        private int amountOfSeats;
+        private decimal priceForOneHour;
+        private byte[] carImage;
+        private Rental rental;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.brand = "BMW";
+            this.model = "5";
+            this.carType = CarType.Sedan;
+            this.carEquipment = new CarEquipment(true, true, true, true);
+            this.driveOfCar = DriveOfCar.Hybrid;
+            this.amountOfDoor = 4;
+            this.amountOfSeats = 5;
+            this.priceForOneHour = 350.00M;
+            this.carImage = new byte[2];
+            this.rental = new Rental();
+        }
+
+
+        [Test]
+        public void CreateCarInstance_IncorrectData_BrandIsNullOrEmpty_ShouldThrowArgumentNullExcenption()
+        {
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => {
+                Car car1 = new Car("", model, carType, carEquipment, 
+                                    driveOfCar, amountOfDoor, amountOfSeats, 
+                                    priceForOneHour, carImage, rental);
+            });
+
+            Assert.Throws<ArgumentNullException>(() => {
+                Car car1 = new Car("", model, carType, carEquipment, 
+                                    driveOfCar, amountOfDoor, amountOfSeats, 
+                                    priceForOneHour, carImage, rental);
+            });
+        }
+
+        [Test]
+        public void CreateCarInstance_IncorrectData_ModelIsNullOrEmpty_ShouldThrowArgumentNullExcenption()
+        {
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => {
+                Car car1 = new Car(brand, "", carType, carEquipment, 
+                                    driveOfCar, amountOfDoor, amountOfSeats, 
+                                    priceForOneHour, carImage, rental);
+            });
+
+            Assert.Throws<ArgumentNullException>(() => {
+                Car car1 = new Car(brand, null!, carType, carEquipment, 
+                                    driveOfCar, amountOfDoor, amountOfSeats, 
+                                    priceForOneHour, carImage, rental);
+            });
+        }
+
+        [Test]
+        public void CreateCarInstance_IncorrectData_EquipmentIsNull_ShouldThrowArgumentNullExcenption()
+        {
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => {
+                Car car1 = new Car(brand, model, carType, null!, 
+                                    driveOfCar, amountOfDoor, amountOfSeats, 
+                                    priceForOneHour, carImage, rental);
+            });
+        }
+
+        [Test]
+        public void CreateCarInstance_IncorrectData_ImageIsNull_ShouldThrowArgumentNullExcenption()
+        {
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => {
+                Car car1 = new Car(brand, model, carType, carEquipment, 
+                                    driveOfCar, amountOfDoor, amountOfSeats, 
+                                    priceForOneHour, null!, rental);
+            });
+        }
+
+        [Test]
+        public void CreateCarInstance_IncorrectData_RentalIsNull_ShouldThrowArgumentNullExcenption()
+        {
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => {
+                Car car1 = new Car(brand, model, carType, carEquipment, 
+                                    driveOfCar, amountOfDoor, amountOfSeats, 
+                                    priceForOneHour, carImage, null!);
+            });
+        }
+
+        [Test]
+        public void CreateCarInstance_CorrectData_ShouldCreateNewInstanceOfCar()
+        {
+           // Act
+           Car newCar = new Car(brand, model, carType, carEquipment, 
+                                    driveOfCar, amountOfDoor, amountOfSeats, 
+                                    priceForOneHour, carImage, rental);
+
+            // Assert
+            Assert.IsInstanceOf<Car>(newCar);
+            Assert.NotNull(newCar);
+        }
+    }
+}
