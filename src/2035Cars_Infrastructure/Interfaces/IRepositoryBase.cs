50ed7a7edaee8f4ae78dbb42753865879bd07fe1
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-25 17:37:58 +0200

    Implement RepositoryBase

--- src/2035Cars_Infrastructure/Interfaces/IRepositoryBase.cs
+++ src/2035Cars_Infrastructure/Interfaces/IRepositoryBase.cs
@@ -4,8 +4,8 @@ namespace _2035Cars_Infrastructure.Interfaces;
 
 public interface IRepositoryBase<T> where T : BaseEntity
 {
-    Task<Guid> CreateAsync(T entity);
+    Task<long> CreateAsync(T entity);
     Task<T> ReadByIDAsync(Guid id);
-    Task<Guid> UpdateAsync(T entity);
-    Task<Guid> DeleteAsync(T entity);
+    Task<long> UpdateAsync(T entity);
+    Task<long> DeleteAsync(T entity);
 }
--- src/2035Cars_Infrastructure/Repositories/RepositoryBase.cs
+++ src/2035Cars_Infrastructure/Repositories/RepositoryBase.cs
@@ -1,44 +1,45 @@
-// using _2035Cars_Core.Domain;
-// using _2035Cars_Infrastructure.Database;
-// using _2035Cars_Infrastructure.Interfaces;
+using _2035Cars_Core.Domain;
+using _2035Cars_Infrastructure.Database;
+using _2035Cars_Infrastructure.Interfaces;
 
-// namespace _2035Cars_Infrastructure.Repositories;
+namespace _2035Cars_Infrastructure.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : BaseEntity
+    {
+        protected readonly CarDbContext _dbContext;
 
-// public class RepositoryBase<T> : IRepositoryBase<T> where T : BaseEntity
-// {
-//     protected readonly CarsDbContext _dbContext;
+        public RepositoryBase(CarDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
-//     public RepositoryBase(CarsDbContext dbContext)
-//     {
-//         _dbContext = dbContext;
-//     }
+        public async Task<long> CreateAsync(T entity)
+        {
+            await this._dbContext.Set<T>().AddAsync(entity);
+            await this._dbContext.SaveChangesAsync();
 
-//     public async Task<Guid> CreateAsync(T entity)
-//     {
-//         await this._dbContext.Set<T>().AddAsync(entity);
-//         await this._dbContext.SaveChangesAsync();
+            return entity.Id;
+        }
 
-//         return entity.Id;
-//     }
+        public async Task<long> DeleteAsync(T entity)
+        {
+            this._dbContext.Set<T>().Remove(entity);
+            await this._dbContext.SaveChangesAsync();
 
-//     public async Task<Guid> DeleteAsync(T entity)
-//     {
-//         this._dbContext.Set<T>().Remove(entity);
-//         await this._dbContext.SaveChangesAsync();
+            return entity.Id;
+        }
 
-//         return entity.Id;
-//     }
+        public async Task<T> ReadByIDAsync(Guid id)
+        {
+            return await this._dbContext.Set<T>().FindAsync(id);
+        }
 
-//     public async Task<T> ReadByIDAsync(Guid id)
-//     {
-//         return await this._dbContext.Set<T>().FindAsync(id);
-//     }
+        public async Task<long> UpdateAsync(T entity)
+        {
+            this._dbContext.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            await this._dbContext.SaveChangesAsync();
 
-//     public async Task<Guid> UpdateAsync(T entity)
-//     {
-//         this._dbContext.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-//         await this._dbContext.SaveChangesAsync();
-
-//         return entity.Id;
-//     }
-// }
+            return entity.Id;
+        }
+    }
+}
