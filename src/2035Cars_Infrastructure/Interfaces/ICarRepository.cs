a2cf4900923c8493c5d35834670816b6d8aeea64
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-27 17:00:23 +0200

    Work with Car Repository

--- src/2035Cars_Infrastructure/Interfaces/ICarRepository.cs
+++ src/2035Cars_Infrastructure/Interfaces/ICarRepository.cs
@@ -1,9 +1,24 @@
 using _2035Cars_Core.Domain;
+using _2035Cars_Core.Enums;
 
 namespace _2035Cars_Infrastructure.Interfaces
 {
     public interface ICarRepository : IRepositoryBase<Car>
     {
-        Task<List<Car>> GetAllNotRentedCarsByCityAsync();
+        Task<List<Car>> GetAllNotRentedCarsByRentalLoactionAsync
+                            (string city, string rentalTitle, int pageNumber, int pageSize);
+
+        Task<List<Car>> GetAllCarsByCarTypeAsync(CarType carType, int pageNumber, int pageSize);
+
+        
+        // TODO: Metoda, która zwraca liste na strone availablecars z warunkami wyszukiwania +
+        // paginacja
+
+        // // // // // // Task<List<Car>> GetAllSelectedCars(string city, 
+        // // // // // //                                     string rentalTitle, 
+        // // // // // //                                     decimal minPrice = 0M, 
+        // // // // // //                                     decimal maxPrice = decimal.MaxValue,
+        // // // // // //                                     bool hasAirConditioning = false,
+        // // // // // //                                     bool has );
     }
 }
--- src/2035Cars_Infrastructure/Repositories/CarRepository.cs
+++ src/2035Cars_Infrastructure/Repositories/CarRepository.cs
@@ -1,6 +1,8 @@
 using _2035Cars_Core.Domain;
+using _2035Cars_Core.Enums;
 using _2035Cars_Infrastructure.Database;
 using _2035Cars_Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace _2035Cars_Infrastructure.Repositories
 {
@@ -10,9 +12,27 @@ namespace _2035Cars_Infrastructure.Repositories
         {
         }
 
-        public async Task<List<Car>> GetAllNotRentedCarsByCityAsync()
+        public async Task<List<Car>> GetAllNotRentedCarsByRentalLoactionAsync
+                            (string city, string rentalTitle, int pageNumber = 1, int pageSize = 5)
         {
-            throw new NotImplementedException();
+            var cars = this._dbContext.Rentals
+            .First(x => string.Equals(x.Address.City, city) && string.Equals(x.Title, rentalTitle))
+            .Cars
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+            return await Task.FromResult(cars);
+        }
+
+        public async Task<List<Car>> GetAllCarsByCarTypeAsync
+                                        (CarType carType, int pageNumber = 1, int pageSize = 5)
+        {
+            return await this._dbContext.Cars
+                                        .Where(x => x.CarType == carType)
+                                        .Skip((pageNumber - 1) * pageSize)
+                                        .Take(pageSize)
+                                        .ToListAsync();
         }
     }
 }
