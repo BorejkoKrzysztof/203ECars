1f683d40c8d134d630537b1324fd51d5373097d3
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-28 17:51:57 +0200

    Add Services Class

---
+++ src/2035Cars_Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,13 @@
+// using Microsoft.Extensions.DependencyInjection;
+
+// namespace _2035Cars_Application
+// {
+//     public static class InfrastructureServicesRegistration
+//     {
+//         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+//         {
+
+//             return services;
+//         }
+//     }
+// }
