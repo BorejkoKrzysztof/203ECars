464eb759eebaea94a1c1ca4e042fcffacf4b12a0
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-28 17:40:11 +0200

    Add CarRepositoryTests

---
+++ Test/InfrastructureTests/CarRepository/CarRepositoryTests.cs
@@ -0,0 +1,11 @@
+namespace Test.InfrastructureTests.BaseRepository
+{
+    public class CarRepositoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+    }
+}
