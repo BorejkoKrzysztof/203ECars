6b403d7d4c2038b488564f917d51aeb81e0b0b7b
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-28 17:39:58 +0200

    Add BaseRepositoryTests

---
+++ Test/InfrastructureTests/BaseRepository/BaseRepositoryTests.cs
@@ -0,0 +1,11 @@
+namespace Test.InfrastructureTests.BaseRepository
+{
+    public class BaseRepositoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+    }
+}
