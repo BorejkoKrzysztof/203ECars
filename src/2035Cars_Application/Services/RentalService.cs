d3f51d35bca8ad818b07da997658f0644e65d858
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-29 20:16:25 +0200

    Upgrade RentalService with tryCatch and logger

--- src/2035Cars_Application/Services/RentalService.cs
+++ src/2035Cars_Application/Services/RentalService.cs
@@ -6,19 +6,46 @@ namespace _2035Cars_Application.Interfaces
     public class RentalService : IRentalService
     {
         private readonly IRentalRepository _repository;
+        private readonly ILogger<RentalService> _logger;
 
-        public RentalService(IRentalRepository repository)
+        public RentalService(IRentalRepository repository, ILogger<RentalService> logger)
         {
             this._repository = repository;
+            this._logger = logger;
         }
         public async Task<List<string>> GetRentalCities()
         {
-            return await this._repository.ReadAllRentalCitiesAsync();
+            List<string> rentalCities;
+            try
+            {
+                rentalCities = await this._repository.ReadAllRentalCitiesAsync();
+                this._logger.LogInformation("List of rental cities is downloaded");
+            }
+            catch (Exception)
+            {
+                _logger.LogError("Unable to download list of rental cities");
+                return null!;
+            }
+
+            return rentalCities;
         }
 
         public async Task<List<string>> GetRentalLocations(string city)
         {
-            return await this._repository.ReadAllLocationsByCityAsync(city);
+            List<string> rentalLocations;
+
+            try
+            {
+                rentalLocations = await this._repository.ReadAllLocationsByCityAsync(city);
+                this._logger.LogInformation("List of rental locations is downloaded");
+            }
+            catch (System.Exception)
+            {
+                _logger.LogError("Unable to download list of rental locations");
+                return null!;
+            }
+
+            return rentalLocations;
         }
     }
 }
