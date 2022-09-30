66326e34fda1e9213f3860a30a6ab0ac3114aa8c
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-30 20:56:48 +0200

    Create RentalService Tests

--- Test/ApplicationTests/RentalService/RentalServiceTests.cs
+++ Test/ApplicationTests/RentalService/RentalServiceTests.cs
@@ -9,12 +9,13 @@ namespace Test.ApplicationTests.RentalService
 {
     public class RentalServiceTests
     {
-        private readonly Mock<IRentalRepository> mockRentalRepository;
-        private readonly IMapper mapper;
-        private readonly ILogger<_2035Cars_Application.Services.RentalService> logger;
-        private readonly _2035Cars_Application.Services.RentalService rentalService;
+        private  Mock<IRentalRepository> mockRentalRepository;
+        private  IMapper mapper;
+        private  ILogger<_2035Cars_Application.Services.RentalService> logger;
+        private  _2035Cars_Application.Services.RentalService rentalService;
 
-        public RentalServiceTests()
+        [SetUp]
+        public void SetUp()
         {
             this.mockRentalRepository = new Mock<IRentalRepository>();
             this.mapper = RentalProfile.Initialize();
@@ -25,15 +26,96 @@ namespace Test.ApplicationTests.RentalService
                                 this.mockRentalRepository.Object, this.logger, this.mapper);
         }
 
-        [SetUp]
-        public void SetUp()
+        [Test]
+        public async Task GetRentalCitiesAsync_NoCitiesInDb_ShouldReturnEmptyCollection()
         {
-            
+            // Arrange
+            List<string> returnedCities = new List<string>();
+
+            this.mockRentalRepository.Setup(x => x.ReadAllRentalCitiesAsync())
+                                        .Returns(Task.FromResult(returnedCities));
+
+            // Act
+            var citiesFromService = await this.rentalService.GetRentalCitiesAsync();
+
+            // Assert
+            Assert.IsInstanceOf<List<string>>(citiesFromService);
+            Assert.That(citiesFromService.Count, Is.EqualTo(0));
         }
 
-        // Task<List<string>> GetRentalCitiesAsync();
-        // Task<List<string>> GetRentalLocationsAsync(string city);
+        [Test]
+        public async Task GetRentalCitiesAsync_NoCitiesInDb_ShouldReturnCollectionWithNamesOfCities()
+        {
+            // Arrange
+            List<string> returnedCities = new List<string>(){
+                "Warszawa",
+                "Poznań",
+                "Kraków",
+                "Gdańsk"
+            };
 
+            this.mockRentalRepository.Setup(x => x.ReadAllRentalCitiesAsync())
+                                        .Returns(Task.FromResult(returnedCities));
+
+            // Act
+            var citiesFromService = await this.rentalService.GetRentalCitiesAsync();
+
+            // Assert
+            Assert.IsInstanceOf<List<string>>(citiesFromService);
+            Assert.That(citiesFromService.Count, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void GetRentalLocationsAsync_IncorrectData_GivenCityIsNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            string city = null!;
+
+            // Assert
+            Assert.ThrowsAsync<ArgumentNullException>(async () => {
+                var rentalLocations = await this.rentalService.GetRentalLocationsAsync(city);
+            });
+        }
+
+        [Test]
+        public async Task GetRentalLocationsAsync_CorrectData_NoLocationsForGivenCity_ShouldReturnEmptyCollection()
+        {
+            // Arrange
+            string givenCity = "Warszawa";
+            var emptyCollection = new List<string>();
+            this.mockRentalRepository.Setup(x => x.ReadAllLocationsByCityAsync(givenCity))
+                                        .ReturnsAsync(emptyCollection);
+
+            // Act
+            var locationsFromService = await this.rentalService.GetRentalLocationsAsync(givenCity);
+
+            // Assert
+            Assert.IsInstanceOf<List<string>>(locationsFromService);
+            Assert.That(locationsFromService.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task GetRentalLocationsAsync_CorrectData__ShouldReturnCollectionWithRentalTitles()
+        {
+            // Arrange
+            string givenCity = "Warszawa";
+            var fakeCollectionResult = new List<string>()
+            {
+                "Lotnisko",
+                "Śródmieście",
+                "Poznań-Ławica"
+            };
+
+            this.mockRentalRepository.Setup(x => x.ReadAllLocationsByCityAsync(givenCity))
+                                        .ReturnsAsync(fakeCollectionResult);
+
+            // Act
+            var locationsFromService = await this.rentalService.GetRentalLocationsAsync(givenCity);
+
+            // Assert
+            Assert.IsInstanceOf<List<string>>(locationsFromService);
+            Assert.That(locationsFromService.Count, Is.EqualTo(3));
+        }
 
     }
 }
