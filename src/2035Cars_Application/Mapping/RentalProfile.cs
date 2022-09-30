9f8e7e0a5168ac1c13c599866d66e2c4bd3e5e09
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-09-30 17:47:49 +0200

    Create Mapping profile

---
+++ src/2035Cars_Application/Mapping/RentalProfile.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace _2035Cars_Application.Mapping
+{
+    public class RentalProfile : Profile
+    {
+        public RentalProfile()
+        {
+            
+        }
+    }
+}
