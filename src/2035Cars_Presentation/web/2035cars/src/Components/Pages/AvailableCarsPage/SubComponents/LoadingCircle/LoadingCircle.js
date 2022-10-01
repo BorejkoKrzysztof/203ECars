f044681bf9d5537db7c4d5ef7c22790108e0e144
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-01 20:22:31 +0200

    Add LoadingCircle Component

--- src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/SubComponents/LoadingCircle/LoadingCircle.js
+++ src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/SubComponents/LoadingCircle/LoadingCircle.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from './LoadingCircle.module.css'
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
 function LoadingCircle() {
   return (
-    <div>LoadingCircle</div>
+    <div className={styles.loadingCircleWrapper}>
+        <p className={styles.circleParagraph}>
+          <AiOutlineLoading3Quarters />
+        </p>
+    </div>
   )
 }
 
