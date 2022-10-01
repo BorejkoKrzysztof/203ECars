77477a4614a62700c6e72fb92872fcb287ff82f1
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-01 12:00:33 +0200

    Add Swagger

--- src/2035Cars_API/Program.cs
+++ src/2035Cars_API/Program.cs
@@ -3,6 +3,7 @@ using _2035Cars_Application;
 using _2035Cars_Infrastructure;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "203E Cars", Version = "v1" });                
+    });
 
 //  Add Authentication
 builder.Services.AddAuthentication(options =>
@@ -60,7 +64,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "203E Cars v1");
+        });
 }
 
 app.UseHttpsRedirection();
