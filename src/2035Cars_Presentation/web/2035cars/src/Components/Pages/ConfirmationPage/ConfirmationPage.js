f58d4074127c4919a9d0cda16165cb1733d9e886
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-07 13:36:30 +0200

    Add Confirmation Page

---
+++ src/2035Cars_Presentation/web/2035cars/src/Components/Pages/ConfirmationPage/ConfirmationPage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from './ConfirmationPage.module.css'
+
+function ConfirmationPage() {
+  return (
+    <div className={styles.confirmationWrapper}>
+        <h1 className={styles.confirmationTitle}>Potwierdź zamówienie</h1>
+        <div className={styles.imageContent}>
+            <img className={styles.carItemImage}
+                                src={process.env.PUBLIC_URL + '/Images/ExampleCars/Jaguar I Pace.png'}
+                                alt={`car`} />
+        </div>
+        <div className={styles.confirmationInfoContent}>
+            <h2>Marka Model</h2>
+            <h2>Od 21.12.22 12:20</h2>
+            <h2>Do 21.12.22 13:30</h2>
+            <h2>Cena 250 PLN</h2>
+            <h2>Imie Nazwisko</h2>
+            <h2>AdresEmail</h2>
+            <h2>Telefon: 111111111</h2>
+        </div>
+        <div className={styles.buttonsContent}>
+            <button className={styles.confirmationButtonResign}>Zrezygnuj</button>
+            <button className={styles.confirmationButtonAccept}>Rezerwuj</button>
+        </div>
+    </div>
+  )
+}
+
+export default ConfirmationPage
