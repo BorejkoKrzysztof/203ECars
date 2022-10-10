070e1c26dd21ee66b303d3909e37bf70594e7537
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-10 15:01:09 +0200

    Add RentalBasicInfo Profile

---
+++ src/2035Cars_Application/DTO/RentalBasicInfo.cs
@@ -0,0 +1,9 @@
+namespace _2035Cars_Application.DTO
+{
+    public class RentalBasicInfo
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string ShortTitle { get; set; }
+    }
+}
--- src/2035Cars_Application/Mapping/RentalProfile.cs
+++ src/2035Cars_Application/Mapping/RentalProfile.cs
@@ -1,3 +1,5 @@
+using _2035Cars_Application.DTO;
+using _2035Cars_Core.Domain;
 using AutoMapper;
 
 namespace _2035Cars_Application.Mapping
@@ -7,7 +9,7 @@ namespace _2035Cars_Application.Mapping
          public static IMapper Initialize()
             => new MapperConfiguration(cfg =>
             {
-               
+               cfg.CreateMap<Rental, RentalBasicInfo>();
             }).CreateMapper();
     }
 }
