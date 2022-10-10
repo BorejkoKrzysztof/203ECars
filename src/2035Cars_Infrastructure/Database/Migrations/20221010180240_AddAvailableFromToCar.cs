127f6cd9fb01de945c02be9ab0778da990024430
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-10 20:03:22 +0200

    AvailableFrom In Car - migration

---
+++ src/2035Cars_Infrastructure/Database/Migrations/20221010180240_AddAvailableFromToCar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2035Cars_Infrastructure.Database.Migrations
+{
+    public partial class AddAvailableFromToCar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RentedTo",
+                table: "Cars",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RentedTo",
+                table: "Cars");
+        }
+    }
+}
