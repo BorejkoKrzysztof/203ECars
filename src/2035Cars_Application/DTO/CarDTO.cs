d2509bc74c4522170f350d9bae98a47c8617f677
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-11 16:14:42 +0200

    Implement CarDTO

--- src/2035Cars_Application/DTO/CarDTO.cs
+++ src/2035Cars_Application/DTO/CarDTO.cs
@@ -2,6 +2,16 @@ namespace _2035Cars_Application.DTO
 {
     public class CarDTO
     {
-        
+        public long CarUniqueReferrence { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal PriceForRental { get; set; }
+        public int AmountOfDoor { get; set; }
+        public int AmountOfSeats { get; set; }
+        public bool HasAirCooling { get; set; }
+        public bool HasHeatingSeats { get; set; }
+        public bool HasAutomaticGearBox { get; set; }
+        public bool HasBuildInNavigation { get; set; }
+        public byte[] Image { get; set; }
     }
 }
