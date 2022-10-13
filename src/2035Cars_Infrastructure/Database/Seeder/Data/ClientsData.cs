6e5e3799b3b2a58acb5121ca3ce61a646f62fda8
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-13 21:33:59 +0200

    Seeder part 2

--- src/2035Cars_Infrastructure/Database/Seeder/Data/ClientsData.cs
+++ src/2035Cars_Infrastructure/Database/Seeder/Data/ClientsData.cs
@@ -1,4 +1,5 @@
 using _2035Cars_Core.Domain;
+using _2035Cars_Core.ValueObjects;
 
 namespace _2035Cars_Infrastructure.Database.Seeder.Data
 {
@@ -8,7 +9,14 @@ namespace _2035Cars_Infrastructure.Database.Seeder.Data
         {
             return new List<Client>()
             {
-
+                new Client()
+                {
+                    EmailAddress = "leone.heathcote@yahoo.com",
+                    Person = new Person("Andrzej", "Popczyk", "642983213"),
+                    Orders = new List<Order>(),
+                    CreatedDate = DateTime.UtcNow,
+                    LastUpdateDate = DateTime.UtcNow
+                }
             };
         }
     }
