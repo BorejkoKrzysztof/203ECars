8e7c9ab89729d43e687e92eb9dd6fbfacb3134be
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-15 13:49:45 +0200

    Add Pages

--- src/2035Cars_Presentation/web/AdminWeb/adminwebservice/src/App.js
+++ src/2035Cars_Presentation/web/AdminWeb/adminwebservice/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import NavbarService from './Components/Navbar/NavbarService';
 import Footer from './Components/Footer/Footer';
+import LoginPage from './Components/Pages/LoginPage/LoginPage'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Register from './Components/Pages/RegisterPage/RegisterPage';
+import EmployeeListPage from './Components/Pages/EmployeeListPage/EmployeeListPage';
 
 function App() {
   return (
-    <>
+    <Router>
       <NavbarService />
-      <div style={{ height: "100vh" }}>
-
-      </div>
+        <Routes>
+          {/* <Route path='/' element={<LoginPage />}/> */}
+          {/* <Route path='/rejestracja' element={<Register />} /> */}
+          <Route path='/' element={<EmployeeListPage />} />
+        </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
