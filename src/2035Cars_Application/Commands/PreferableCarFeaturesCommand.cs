b9225e5c1800654bfac2e9ee3760eb9994b7b234
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-21 21:48:40 +0200

    Change name of some variables

--- src/2035Cars_Application/Commands/PreferableCarFeaturesCommand.cs
+++ src/2035Cars_Application/Commands/PreferableCarFeaturesCommand.cs
@@ -3,21 +3,21 @@ namespace _2035Cars_Application.Commands
     public class PreferableCarFeaturesCommand
     {
         public DateTime AvailableFrom { get; set; }
-        public DateTime OrderFrom { get; set; }
+        // public DateTime OrderFrom { get; set; }
         public DateTime OrderTo { get; set; }
 
         public decimal MinimumPrice { get; set; }
         public decimal MaximumPrice { get; set; }
 
-        public bool desiredSuvType { get; set; }
-        public bool desiredSportType { get; set; }
-        public bool desiredConvertibleType { get; set; }
-        public bool desiredSedanType { get; set; }
+        public bool DesiredSuvType { get; set; }
+        public bool DesiredSportType { get; set; }
+        public bool DesiredCompactType { get; set; }
+        public bool DesiredSedanType { get; set; }
 
         public bool DesiredAirCooling { get; set; }
         public bool DesiredHeatingSeats { get; set; }
         public bool DesiredAutomaticGearBox { get; set; }
-        public bool DesiredBuilInNavigation { get; set; }
+        public bool DesiredBuildInNavigation { get; set; }
 
         public bool DesiredHybridDrive { get; set; }
         public bool DesiredElectricDrive { get; set; }
