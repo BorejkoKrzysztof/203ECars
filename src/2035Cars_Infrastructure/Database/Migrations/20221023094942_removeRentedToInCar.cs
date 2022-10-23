1c5127d379e94b8e2046a55cba8f57fe48aa071d
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-23 20:11:44 +0200

    Add new migration

---
+++ src/2035Cars_Infrastructure/Database/Migrations/20221023094942_removeRentedToInCar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2035Cars_Infrastructure.Database.Migrations
+{
+    public partial class removeRentedToInCar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RentedTo",
+                table: "Cars");
+
+            migrationBuilder.CreateTable(
+                name: "ContactMessages",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReservationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactMessages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactMessages");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RentedTo",
+                table: "Cars",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
