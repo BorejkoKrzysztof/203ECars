7f66514b0362d6846b5cb13d6d1a712e3a5ddc62
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-23 20:10:18 +0200

    Add new command

---
+++ src/2035Cars_Application/Commands/CarsByTypeCommand.cs
@@ -0,0 +1,10 @@
+namespace _2035Cars_Application.Commands
+{
+    public class CarsByTypeCommand
+    {
+        public bool DesiredSuvType { get; set; }
+        public bool DesiredSportType { get; set; }
+        public bool DesiredCompactType { get; set; }
+        public bool DesiredSedanType { get; set; }
+    }
+}
