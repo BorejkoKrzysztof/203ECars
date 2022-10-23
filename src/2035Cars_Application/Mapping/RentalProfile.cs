98c753aaea1714ad291b2e9fe15a5e26dea1b272
Krzysztof Borejko <krzysiek.13.krzysiek@gmail.com>
2022-10-23 20:10:50 +0200

    Create map for Car and CarDTO

--- src/2035Cars_Application/Mapping/RentalProfile.cs
+++ src/2035Cars_Application/Mapping/RentalProfile.cs
@@ -6,10 +6,17 @@ namespace _2035Cars_Application.Mapping
 {
     public class RentalProfile : Profile
     {
-         public static IMapper Initialize()
-            => new MapperConfiguration(cfg =>
-            {
+        public static IMapper Initialize()
+           => new MapperConfiguration(cfg =>
+           {
                cfg.CreateMap<Rental, RentalBasicInfo>();
-            }).CreateMapper();
+               cfg.CreateMap<Car, CarDTO>()
+                            .ForMember(x => x.HasAirCooling, m => m.MapFrom(c => c.Equipment.HasAirCooling))
+                            .ForMember(x => x.HasAutomaticGearBox, m => m.MapFrom(c => c.Equipment.HasAutomaticGearBox))
+                            .ForMember(x => x.HasBuildInNavigation, m => m.MapFrom(c => c.Equipment.HasBuildInNavigation))
+                            .ForMember(x => x.HasHeatingSeats, m => m.MapFrom(c => c.Equipment.HasHeatingSeat))
+                            .ForMember(x => x.PriceForRental, m => m.MapFrom(c => c.PriceForOneHour))
+                            .ReverseMap();
+           }).CreateMapper();
     }
 }
