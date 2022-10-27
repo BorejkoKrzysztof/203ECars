3e2e166ebc7faa17f895ca1551cec129b5eac7fc
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-10-27 20:46:49 +0200

    Fir CarFeatures Form Finally - part 3 - FINISH

--- src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/AvailableCarsPage.js
+++ src/2035Cars_Presentation/web/2035cars/src/Components/Pages/AvailableCarsPage/AvailableCarsPage.js
@@ -356,12 +356,9 @@ function AvailableCarsPage() {
   useEffect(() => {
       if (resetedFromCarFeaturesForm) {
         setCookiesFromCarEquipmentData()
-        console.log('pierwszy IF')
         if (cityFrom !== cityDefaultValue || locationFrom !== locationDefaultValue) {
-          console.log('drugi IF')
           downloadCarsByLocationFrom()
         } else {
-          console.log('ELSE')
           downloadAllCars()
         }
         
