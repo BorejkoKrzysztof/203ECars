c5917590b4d49b68eef0e84418e53433235d3d01
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-10-29 21:18:19 +0200

    Create authorized axios

---
+++ src/2035Cars_Presentation/web/AdminWeb/adminwebservice/axios/axiosAuthorize.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
+import dayjs from 'dayjs'
+
+const baseURL = process.env.REACT_APP_SERVER;
+
+const instance = axios.create({
+                    baseURL: `${baseURL}`,
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json; charset=utf-8'
+                    }
+                })
+
+
+instance.interceptors.request.use(async (req) => {
+    const jwt = JSON.parse(localStorage.getItem('token'));
+
+    const user = jwtDecode(jwt);
+    const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+
+    if (!isExpired)
+    {
+        req.headers.common.Authorization = `Bearer ${jwt}`;
+        return req;
+    }
+    else 
+    {
+        const refreshToken = JSON.parse(localStorage.getItem('refreshToken'))
+        const response = await axios.post(`${baseURL}/account/refreshtoken`, {
+            RefreshToken : refreshToken
+        })
+
+        localStorage.setItem('token', JSON.stringify(response.data.token))
+        req.headers.common.Authorization = `Bearer ${response.data.token}`
+        return req;
+    }
+})
+
+export default instance;
