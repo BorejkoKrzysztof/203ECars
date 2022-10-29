65145f3df38ec10bfd8a976661e171b48b958b51
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-10-29 21:23:47 +0200

    Import axios in register page component

--- src/2035Cars_Presentation/web/AdminWeb/adminwebservice/src/Components/Pages/RegisterPage/RegisterPage.js
+++ src/2035Cars_Presentation/web/AdminWeb/adminwebservice/src/Components/Pages/RegisterPage/RegisterPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styles from './RegisterPage.module.css'
-// import axios from '../../../axios/axiosPublicInstance'
+import axios from '../../../axios/axiosDefault'
 import { RiErrorWarningLine } from 'react-icons/ri'
 
 
