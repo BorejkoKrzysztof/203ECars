2c3333b88ba4620cd0700786cfdc009862a4a318
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-10-30 21:12:49 +0100

    Register new dependencies

--- src/2035Cars_Application/ApplicationServicesRegistration.cs
+++ src/2035Cars_Application/ApplicationServicesRegistration.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using _2035Cars_Application.Interfaces;
 using _2035Cars_Application.Mapping;
 using _2035Cars_Application.Services;
+using _2035Cars_Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace _2035Cars_Application
@@ -11,11 +12,12 @@ namespace _2035Cars_Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddSingleton(RentalProfile.Initialize());
+            services.AddSingleton<IJwtHandler, JwtHandler>();
             services.AddScoped<IRentalService, RentalService>();
             services.AddScoped<ICarService, CarService>();
             services.AddScoped<IOrderService, OrderService>();
             services.AddScoped<IOrderService, OrderService>();
-            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IEmployeeService, EmployeeService>();
 
             return services;
         }
