452de6982248ba8bde7eb727283ae8520c5d72e2
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-10-30 21:14:53 +0100

    Add methods to Employee Repository

--- src/2035Cars_Infrastructure/Interfaces/IEmployeeRepository.cs
+++ src/2035Cars_Infrastructure/Interfaces/IEmployeeRepository.cs
@@ -1,9 +1,17 @@
 using _2035Cars_Core.Domain;
+using _2035Cars_Core.Enums;
+using _2035Cars_Core.ValueObjects;
 
 namespace _2035Cars_Infrastructure.Interfaces
 {
     public interface IEmployeeRepository : IRepositoryBase<Employee>
     {
-        
+        Task<bool> IsAccountExisting(string emailAddress);
+        Task<Account> ReadAccount(string emailAddress);
+        Task<long> GetEmployeeIdByEmailAddress(string emailAddress);
+        Task<BuisnessPosition> GetEmployeePositionById(long employeeId);
+        Task<string> GetRefreshToken(long employeeId);
+        Task<string?> CreateRefreshToken(long employeeId);
+        Task<long> GetUserIdByRefreshToken(string refreshToken);
     }
 }
--- src/2035Cars_Infrastructure/Repositories/EmployeeRepository.cs
+++ src/2035Cars_Infrastructure/Repositories/EmployeeRepository.cs
@@ -1,4 +1,6 @@
 using _2035Cars_Core.Domain;
+using _2035Cars_Core.Enums;
+using _2035Cars_Core.ValueObjects;
 using _2035Cars_Infrastructure.Database;
 using _2035Cars_Infrastructure.Interfaces;
 
@@ -8,6 +10,41 @@ namespace _2035Cars_Infrastructure.Repositories
     {
         public EmployeeRepository(CarDbContext dbContext) : base(dbContext)
         {
+        }
+
+        public Task<string?> CreateRefreshToken(long employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<long> GetEmployeeIdByEmailAddress(string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BuisnessPosition> GetEmployeePositionById(long employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRefreshToken(long employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<long> GetUserIdByRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAccountExisting(string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> ReadAccount(string emailAddress)
+        {
+            throw new NotImplementedException();
         }
     }
 }
